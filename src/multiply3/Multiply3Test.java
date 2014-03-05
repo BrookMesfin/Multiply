facd609f2fa0d820a64ec59d6011fa84960c7b79
W0985353 <W0985353@BS145B1-4.crc.ad.losrios.edu>
2014-03-04 21:49:35 -0800

    Merge origin/master
    Conflicts:
    	src/additionapplet/AdditionApplet.java

--- src/multiply3/Multiply3Test.java
+++ src/multiply3/Multiply3Test.java
@@ -4,10 +4,14 @@ import java.util.*;
 
 public class Multiply3Test {
     
-    static int answer;
     static int numbers1;
     static int numbers2;
-    
+    static int answer;
+    static int input;
+    static int guess;
+    static String response; 
+    static Random randomNumber = new Random();
+       
      public static void main(String[] args) {
         
          quiz();
@@ -15,47 +19,64 @@ public class Multiply3Test {
      
      public static void quiz(){
          createQuestion();
+         checkResponse();
+         
          
      }
      
      public static void createQuestion(){
          Scanner input = new Scanner(System.in);
-         Random multiplyNumber = new Random();
-         numbers1 = multiplyNumber.nextInt(10);// genrate random numbers between 0-9
-         numbers2 = multiplyNumber.nextInt(10);// genrate random numbers between 0-9
+         numbers1 = randomNumber.nextInt(10);// genrate random numbers between 0-9
+         numbers2 = randomNumber.nextInt(10);// genrate random numbers between 0-9
          answer = numbers1 * numbers2;//calculates answer
          
          System.out.println("How much is "+numbers1+" times "
                 +numbers2+"?");
          
-         input.nextInt();
+         guess = input.nextInt();
          
      }
      
-     public String createResponse(boolean answer){
-         
-       switch (answer) {
-             case 1: return ("Very good!");
-              
-             case 2: return ("Excellent!");
-             
-             case 3:return ("Nice work!");
-             
-             case 4: return ("Keep up the good work!");
+     public static String createResponse(boolean correct){
+      
+      if (correct) { 
+      
+        switch (randomNumber.nextInt(4)+1) {
+             case 1: return response = ("Very good!");
+                               
+             case 2: return response = ("Excellent!");
+                 
+             case 3: return response = ("Nice work!");
+                 
+             case 4: return response = ("Keep up the good work!");
+                
           }
-         switch (answer){
-             case 1: return ("No. Please try again.");
+    }       
+   
+              switch (randomNumber.nextInt(4)+1){
+             case 1: return response = ("No. Please try again.");
                  
-             case 2: return ("Wrong. Try once more.");
+             case 2: return response = ("Wrong. Try once more.");
                  
-             case 3: return ("Don't give up!");
+             case 3: return response = ("Don't give up!");
                  
-             case 4: return (" No. Keep trying.");
+             case 4: return response = (" No. Keep trying.");
+             
          }
-               
-     }
      
-     public void checkResponse(){
+        return response;
+ }
+     
+     public static void checkResponse(){
+         
+         if (answer == guess){
+             
+            System.out.println(createResponse(true));
+             
+         }else{
+             
+             System.out.println(createResponse(false));
+         }
          
      }
      
