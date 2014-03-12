fbc018e49bd5ede2b1afdfd877b737eec8fc6af5
W0985353 <W0985353@BS145B1-4.crc.ad.losrios.edu>
2014-03-11 20:19:44 -0700

    Merge origin/master
    Conflicts:
    	src/additionapplet/AdditionApplet.java

--- src/multiply3/Multiply3.java
+++ src/multiply3/Multiply3.java
@@ -1,8 +1,14 @@
 package multiply3;
 
+import static multiply3.Multiply3Test.quiz;
+
 
 public class Multiply3 {
 
-   
+        public static void main(String[] args) {
+        
+        Multiply3Test test = new Multiply3Test();
+            test.quiz();
+    }
     
 }
